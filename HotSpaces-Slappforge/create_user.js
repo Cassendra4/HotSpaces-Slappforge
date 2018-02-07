a54245e1b5e78fc7007a8504cb8e020adb6b4347
Hasangi Thathsarani <hasangik.12@cse.mrt.ac.lk>
2018-02-07 17:17:22 +0530

    adding create user

--- HotSpaces-Slappforge/create_user.js
+++ HotSpaces-Slappforge/create_user.js
@@ -16,57 +16,8 @@ exports.handler = function (event, context, callback) {
 	var lat = JSON.parse(event.body).last_known_lat;
 	var long = JSON.parse(event.body).last_known_long;
 	var currentTime = currentTimestamp.toLocaleString();
-	ddb.put({
-		TableName: 'hs_user',
-		Item: {
-			'username': username,
-			'email': email,
-			'first_name': firstName,
-			'last_name': lastName,
-			'age': age,
-			'gender': gender,
-			'interested_in': interestedIn,
-			'password': password,
-			'user_avatar': userAvatar,
-			'contact_number': contactNumber
-		}
-	}, function (err, data) {
-		console.log('err', err);
-		console.log('data', data);
-		if (err) {
-			callback(err, null);
-		} else {
-
-			ddb.put({
-				TableName: 'hs_sort_table',
-				Item: {
-					'gender': gender,
-					'username': username,
-					'last_known_lat': lat,
-					'last_updated_timestamp': currentTime,
-					'last_known_long': long
-				}
-			}, function (err, data) {
-				console.log('err', err);
-				console.log('data', data);
-				if (err) {
-					callback(err, null);
-				} else {
-					let response = {
-						"statusCode": 200,
-						"headers": {
-							"my_header": "my_value"
-						},
-						"body": JSON.stringify(data),
-						"isBase64Encoded": false
-					};
-					callback(null, response);
-				}
-			});
 
 
-		}
-	});
 
 	callback(null, 'Successfully executed');
 }
